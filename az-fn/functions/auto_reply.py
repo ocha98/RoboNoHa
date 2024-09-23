cb428fa08fef294faaf2bd7af09b3f3906342890
ocha98 <48848793+ocha98@users.noreply.github.com>
2024-09-23 10:29:45 +0900

    [Fix] az-fn logging and bluesky login

--- az-fn/functions/auto_reply.py
+++ az-fn/functions/auto_reply.py
@@ -2,15 +2,12 @@ import os
 import boto3
 from atproto import Client, models
 import markovify
+import logging
 
 import azure.functions as func
 
-
 S3_BUCKET = os.getenv('S3_BUCKET')
 CHAIN_FILE_KEY = os.getenv('CHAIN_FILE_KEY')
-
-BLUESKY_HANDLE = os.getenv('BLUESKY_HANDLE')
-BLUESKY_APP_PASS = os.getenv('BLUESKY_APP_PASS')
 BLUESKY_SESSION = os.getenv('BLUESKY_SESSION')
 
 def load_model_data(s3, bucket: str, key: str) -> str:
@@ -19,12 +16,15 @@ def load_model_data(s3, bucket: str, key: str) -> str:
 
 bp = func.Blueprint()
 
-@bp.schedule(schedule="0 * * * * *", arg_name = "autoReplyFunc", run_on_startup = False, use_monitor = False)
+@bp.schedule(schedule='0 * * * * *', arg_name = 'autoReplyFunc', run_on_startup = False, use_monitor = False)
 def auto_reply(autoReplyFunc: func.TimerRequest):
-    print("auto_reply start")
-    bsky_client = Client()
-    bsky_client.login(BLUESKY_HANDLE, BLUESKY_APP_PASS, session_string = BLUESKY_SESSION)
+    logging.info('auto_reply start')
 
+    logging.info('login to bluesky')
+    bsky_client = Client()
+    bsky_client.login(session_string = BLUESKY_SESSION)
+
+    logging.info('getting notifications')
     res = bsky_client.app.bsky.notification.list_notifications()
     need_to_reply = []
     for notification in res.notifications:
@@ -41,18 +41,23 @@ def auto_reply(autoReplyFunc: func.TimerRequest):
         need_to_reply.append((parent, root))
 
     if len(need_to_reply) == 0:
-        print("auto_reply end")
+        logging.info('no need to reply')
         return
 
-    s3 = boto3.client(service_name ="s3")
-    model_json = load_model_data(s3, S3_BUCKET, CHAIN_FILE_KEY)
-
+    logging.info(f'need to reply len: {len(need_to_reply)}')
+    logging.info('update seen')
     bsky_client.app.bsky.notification.update_seen({'seen_at': bsky_client.get_current_time_iso()})
 
+    logging.info('loading model data')
+    s3 = boto3.client(service_name ='s3')
+    model_json = load_model_data(s3, S3_BUCKET, CHAIN_FILE_KEY)
     model = markovify.Text.from_json(model_json)
+
+    logging.info('sending posts')
     for parent, root in need_to_reply:
         random_post = model.make_short_sentence(60).replace(' ', '')
         reply_to = models.AppBskyFeedPost.ReplyRef(parent = parent, root = root)
-        bsky_client.send_post(text = random_post, reply_to = reply_to)
+        logging.info(f'replying to {reply_to}')
+        bsky_client.send_post(text = random_post, reply_to = reply_to, langs = ['ja'])
 
-    print("auto_reply end")
+    logging.info('auto_reply end')
--- az-fn/functions/random_post.py
+++ az-fn/functions/random_post.py
@@ -2,38 +2,35 @@ import os
 import boto3
 import markovify
 from atproto import Client
-
+import logging
 import azure.functions as func
-
 
 S3_BUCKET = os.getenv('S3_BUCKET')
 CHAIN_FILE_KEY = os.getenv('CHAIN_FILE_KEY')
-
-BLUESKY_HANDLE = os.getenv('BLUESKY_HANDLE')
-BLUESKY_APP_PASS = os.getenv('BLUESKY_APP_PASS')
 BLUESKY_SESSION = os.getenv('BLUESKY_SESSION')
 
 def load_model_data(s3, bucket: str, key: str) -> str:
     data = s3.get_object(Bucket = bucket, Key = key)['Body'].read().decode('utf-8')
     return data
 
-def upload_model_data(s3, bucket: str, key: str, data: str) -> None:
-    s3.put_object(Bucket = bucket, Key = key, Body = data)
-
-
 bp = func.Blueprint()
 
-@bp.schedule(schedule="0 0 */4 * * *", arg_name = "randomPostFunc", run_on_startup = False, use_monitor = False)
+@bp.schedule(schedule='0 0 */4 * * *', arg_name = 'randomPostFunc', run_on_startup = False, use_monitor = False)
 def random_post(randomPostFunc: func.TimerRequest):
-    print("random_post start")
-    s3 = boto3.client(service_name ="s3")
+    logging.info('random_post start')
+    s3 = boto3.client(service_name ='s3')
     bsky_client = Client()
-    bsky_client.login(BLUESKY_HANDLE, BLUESKY_APP_PASS, session_string = BLUESKY_SESSION)
 
+    logging.info('login to bluesky')
+    bsky_client.login(session_string = BLUESKY_SESSION)
+
+    logging.info('loading model data')
     model_json = load_model_data(s3, S3_BUCKET, CHAIN_FILE_KEY)
-
     model = markovify.Text.from_json(model_json)
+
     random_post = model.make_short_sentence(60).replace(' ', '')
-    print("random_post send")
-    bsky_client.send_post(text = random_post)
-    print("random_post end")
+
+    logging.info('sending post')
+    bsky_client.send_post(text = random_post, langs = ['ja'])
+
+    logging.info('random_post end')
