e899c6777b7ff3d2f6fee29b2354fbb48b3479d0
ocha98 <48848793+ocha98@users.noreply.github.com>
2024-09-25 13:14:09 +0900

    [Update] memory cache

--- az-fn/functions/lib.py
+++ az-fn/functions/lib.py
@@ -3,6 +3,7 @@ import os
 from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
 from atproto import Client
+from atproto.exceptions import BadRequestError
 import boto3
 import markovify
 
@@ -12,24 +13,43 @@ KEY_VAULT_URL = os.getenv('KEY_VAULT_URL')
 BLUESKY_SESSION_KEYVAULT_NAME = os.getenv('BLUESKY_SESSION_KEYVAULT_NAME')
 
 class BlueSkySession:
+    _bluesky_session: str = None
+
     def __init__(self) -> None:
         credential = DefaultAzureCredential()
         self.secret_client = SecretClient(vault_url = KEY_VAULT_URL, credential = credential)
 
     def __enter__(self):
-        logging.info('getting bluesky session from keyvalut...')
-        self._bluesky_session = self.secret_client.get_secret(BLUESKY_SESSION_KEYVAULT_NAME).value
+        if BlueSkySession._bluesky_session is None:
+            logging.info('getting bluesky session from keyvalut...')
+            BlueSkySession._bluesky_session = self.secret_client.get_secret(BLUESKY_SESSION_KEYVAULT_NAME).value
+        else:
+            logging.info('using cached bluesky session')
 
         logging.info('login to bluesky...')
         self.bluesky_client = Client()
-        self.bluesky_client.login(session_string = self._bluesky_session)
+
+        try:
+            self.bluesky_client.login(session_string = BlueSkySession._bluesky_session)
+        except BadRequestError as e:
+            if e.response.status_code == 400 and e.response.content.error == 'ExpiredToken':
+                logging.info('session is expired')
+                logging.info('taking new session from keyvault...')
+                BlueSkySession._bluesky_session = self.secret_client.get_secret(BLUESKY_SESSION_KEYVAULT_NAME).value
+
+                logging.info('retry login to bluesky...')
+                self.bluesky_client.login(session_string = BlueSkySession._bluesky_session)
+            else:
+                raise e
+
         return self.bluesky_client
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         logging.info('checking bluesky session is updated...')
         new_session = self.bluesky_client.export_session_string()
 
-        if new_session != self._bluesky_session:
+        if new_session != BlueSkySession._bluesky_session:
+            BlueSkySession._bluesky_session = new_session
             logging.info('session is updated !')
             logging.info('updating bluesky session of keyvault...')
             self.secret_client.set_secret(BLUESKY_SESSION_KEYVAULT_NAME, new_session)
